b4e876ba2c30b3705719ae1ef876eef459097f62
pchainho <pchainho@gmail.com>
2017-04-09 19:07:10 +0100

    preparing development of my context

--- examples/main.js
+++ examples/main.js
@@ -19,26 +19,6 @@ export function hypertyDeployed(hyperty) {
   let script = '';
 
   switch (hyperty.name) {
-    case 'Discovery':
-      template = 'discovery/discovery';
-      script = 'discovery/dist/discovery.js';
-      break;
-
-    case 'QBWebRTC':
-      template = 'qbwebrtc/qbwebrtc';
-      script = 'qbwebrtc/dist/qbwebrtc.js';
-      break;
-
-    case 'Connector':
-      template = 'connector/Connector';
-      script = 'connector/demo.js';
-      break;
-
-    case 'GroupChatManager':
-      template = 'group-chat-manager/ChatManager';
-      script = 'group-chat-manager/demo.js';
-      break;
-
     case 'HelloWorldObserver':
       template = 'hello-world/helloWorld';
       script = 'hello-world/helloObserver.js';
@@ -48,47 +28,10 @@ export function hypertyDeployed(hyperty) {
       template = 'hello-world/helloWorld';
       script = 'hello-world/helloReporter.js';
       break;
-
-    case 'SurveyReporter':
-      template = 'survey/surveyReporter';
-      script = 'survey/surveyReporter.js';
-      break;
-
-    case 'SurveyObserver':
-      template = 'survey/surveyObserver';
-      script = 'survey/surveyObserver.js';
-      break;
-
-    case 'GroupChat':
-      template = 'group-chat/groupChat';
-      script = 'group-chat/groupChat.js';
-      break;
-
-    case 'NotificationsReporter':
-      template = 'notifications/notificationsReporter';
-      script = 'notifications/notificationsReporter.js';
-      break;
-
-    case 'NotificationsObserver':
-      template = 'notifications/notificationsObserver';
-      script = 'notifications/notificationsObserver.js';
-      break;
-
     case 'Location':
       template = 'location/location';
       script = 'location/location.js';
       break;
-
-    case 'RoomClient':
-      template = 'room-ui/roomClient';
-      script = 'room-ui/roomClient.js';
-      break;
-
-    case 'RoomServer':
-      template = 'room-ui/roomServer';
-      script = 'room-ui/roomServer.js';
-      break;
-
     case 'UserStatus':
       template = 'user-status/UserStatus';
       script = 'user-status/user-status.js';
@@ -98,16 +41,12 @@ export function hypertyDeployed(hyperty) {
       template = 'bracelet/bracelet';
       script = 'bracelet/BraceletSensorObserver.js';
       break;
-
-    case 'DTWebRTC':
-      template = 'dtwebrtc/dtwebrtc';
-      script = 'dtwebrtc/dtwebrtc.js';
+    case 'MyContext':
+      template = 'myContext/myContext';
+      script = 'myContext/MyContext.js';
       break;
 
-    case 'NodeHypertyObserver':
-      template = 'node-hyperty/NodeHyperty';
-      script = 'node-hyperty/NodeHypertyObserver.js';
-      break;
+
   }
 
   if (!template) {
